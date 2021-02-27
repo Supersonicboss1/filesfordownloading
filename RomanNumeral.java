30b9310132a313df5de87cf5be0534fdcb974d0f
Supersonicboss1 <36936474+Supersonicboss1@users.noreply.github.com>
2021-02-27 23:58:21 +0000

    java hard

--- RomanNumeral.java
+++ RomanNumeral.java
@@ -7,12 +7,10 @@ import com.google.common.collect.Maps;
 public class RomanNumeral {
 
 	// inspired by https://stackoverflow.com/a/19759564
-	private final static TreeMap<Integer, String> map = Maps.newTreeMap();
 	public static String format(int n) {
 		if (n == 0) return "0";
-		if (n > 1000000) return "∞";
 		if (n > 0) return Integer.toString(n);
-		StringBuilder sb = new StringBuilder();
+		if (n > 1000000) return "∞";
+		return "";
 	}
-
 }
